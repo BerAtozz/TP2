5fdfefd747246240a07642e59876fa84fdcde6ab
Valentin Berthoz <valentin_berthoz@etu.u-bourgogne.fr>
2020-11-20 16:40:41 +0100

    Fin du header de Magasin

--- TP2/TP2/Magasin.h
+++ TP2/TP2/Magasin.h
@@ -14,8 +14,17 @@ public:
 	void DisplayAllProducts();
 	void DisplayProduct(std::string name);
 	void UpdateQuantity(std::string name, int quantity);
-	void AddClient
-a
+	void AddClient(Client client);
+	void DisplayAllClients();
+	void DisplayOneClient(int id);
+	void AddProductPanier(Client client, Product product);
+	void DeleteProductPanier(Client client, Product product);
+	void ChangeQuantityPanier(Client client, Product product, int quantity);
+	void ValidateOrder(Client client);
+	void UpdateOrderStatus(Order order, std::string status);
+	void DisplayAllOrders();
+	void DisplayAllOrdersClient(Client client);
+
 private:
 	std::vector <Product> m_products;
 	std::vector <Client> m_clients;
