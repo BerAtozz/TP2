bf25467522f4c678f60df6c812525e9cfc77b52b
GEKO-SAN <husenechal@gmail.com>
2020-11-20 16:21:51 +0100

    ajout des fonctions membres et methode dans product.h et confection des methode dans product.cpp

--- TP2/TP2/Product.cpp
+++ TP2/TP2/Product.cpp
@@ -1,1 +1,35 @@
 #include "Product.h"
+#include <string>
+
+Product(std::string title, std::string descrption, int quantity, double price);
+{
+  m_title = title;
+  m_descrption = descrption;
+  m_quantity = quantity;
+  m_price = price;
+}
+
+std::string getTitle();
+{
+  return m_title;
+}
+
+std::string getDescription();
+{
+  return m_descrption;
+}
+
+int getQuantity();
+{
+  return m_quantity;
+}
+
+double getPrice();
+{
+  return m_price;
+}
+
+void ChangeQuantity(int quantity);
+{
+  m_quantity = quantity;
+}
